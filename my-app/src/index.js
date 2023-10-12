56b381f09cd38cf3c9dffa4a8e78b1138e763901
echung32 <143231894+echung32@users.noreply.github.com>
2023-10-11 21:09:15 -1000

    finish

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,18 +2,111 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
-import {Container, NavLink, Stack} from "react-bootstrap";
-import {Apple, ArrowRight, Facebook, Instagram, Phone, Tiktok, Twitter} from "react-bootstrap-icons";
+import {Button, Container, Image, Nav, Navbar, NavDropdown, NavLink, Stack} from "react-bootstrap";
+import {
+    Apple,
+    ArrowRight,
+    CaretRight,
+    Cart,
+    Facebook,
+    Instagram,
+    Map,
+    Person,
+    Phone,
+    Tiktok,
+    Twitter
+} from "react-bootstrap-icons";
 
-const Navbar = () => (<></>)
+const Navigation = () => (<Container fluid className={"px-0"}>
+    <Navbar expand="lg" bg="light" className={"shadow"}>
+        <Container className={"py-1"}>
+            <Nav className={"align-items-center"}>
+                <Navbar.Brand className={"justify-content-center"} href="#">
+                    <Image src={"/img/logo.webp"}
+                           width="59px" height="47px"/>
+                </Navbar.Brand>
+                <Nav.Link><h4 className={"fw-bold"}>Deals</h4></Nav.Link>
+                <NavDropdown title={"Menu"} className={"h4 fw-bold"}>
+                    <NavDropdown.Item>Pizza</NavDropdown.Item>
+                    <NavDropdown.Item>Wings</NavDropdown.Item>
+                    <NavDropdown.Item>Sides</NavDropdown.Item>
+                    <NavDropdown.Item>Pasta</NavDropdown.Item>
+                    <NavDropdown.Item>Desserts</NavDropdown.Item>
+                    <NavDropdown.Item>Drinks</NavDropdown.Item>
+                    <NavDropdown.Item>Dips</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+            <Nav className={"justify-content-end gap-3 align-items-center"}>
+                <Stack direction={"horizontal"} className={"shadow p-2 rounded"} gap={2}>
+                    <Person className={"h1 mt-1"}/>
+                    <Stack>
+                        <span className="fw-light red-color"><small>HUT REWARDS</small></span>
+                        <span>Sign In <ArrowRight/></span>
+                    </Stack>
+                </Stack>
+                <NavDropdown id="no-caret" title={<Stack direction={"horizontal"} className={"h4 fw-bold"} gap={1}><Cart/>$0.00</Stack>}>
+                    <NavDropdown.Item>Empty Cart</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+        </Container>
+    </Navbar>
+</Container>)
 
-const CenterImage = () => (<></>)
+const CenterImage = () => (<NavLink>
+    <Image src={"/img/big-dinner-box.avif"} alt={"PizzaHut Promo"} className={"w-100 rounded shadow"}/>
+</NavLink>)
 
-const StartHere = () => (<></>)
+const StartHere = () => (
+    <Stack direction={"horizontal"} className={"justify-content-around rounded shadow p-3 align-items-center"}
+           id={"border-red"}>
+        <Stack direction={"horizontal"} id={"right-border-line"} className={"h2 young-serif"} gap={1}>
+            <Map/>
+            <span className={"pe-5"}> Start here</span>
+        </Stack>
+        <span className="h5 pt-1">Find your store to see local deals</span>
+        <Button variant={"danger"} className="rounded fw-bold px-5" id="white-color">FIND DEALS</Button>
+    </Stack>)
 
-const ItemCard = () => (<></>)
+const ItemCard = () => (<Stack className={"rounded shadow"}>
+    <Image src={"/img/item-1.jpg"} className={"w-100 rounded-top"} alt={"PizzaHut"}/>
+    <Stack direction={"horizontal"} className={"align-items-center p-3"} gap={3}>
+        <Stack>
+            <strong>$6.99 Pizza Hut Melts</strong>
+            <small>Crispy. Dippable. Loaded with toppings & cheese.</small>
+        </Stack>
+        <NavLink href={"#"}><h3><CaretRight/></h3></NavLink>
+    </Stack>
+</Stack>)
 
-const ItemCardLong = () => (<></>)
+const ItemCardLong = () => (<Stack className={"rounded shadow"}>
+    <Image src={"/img/item-2.avif"} className={"w-100 rounded-top"} alt={"PizzaHut"}/>
+    <Stack direction={"horizontal"} className={"align-items-center p-3"} gap={3}>
+        <Stack className={"w-100"}>
+            <strong>$12.99 Original Stuffed Crust®</strong>
+            <small>Nothing beats the original.</small>
+        </Stack>
+        <NavLink href={"#"}><h3><CaretRight/></h3></NavLink>
+    </Stack>
+</Stack>)
+
+const Featured = () => (<Stack gap={3}>
+    <Stack direction={"horizontal"} gap={3}>
+        <h1 className="young-serif">Featured</h1>
+        <NavLink>
+            <small className="red-color fw-bold">SEE MORE </small>
+            <ArrowRight/>
+        </NavLink>
+    </Stack>
+    <Stack direction={"horizontal"} gap={3} className={"justify-content-around"}>
+        <ItemCard/>
+        <ItemCard/>
+        <ItemCard/>
+    </Stack>
+    <Stack direction={"horizontal"} gap={3} className={"justify-content-around mt-3"}>
+        <ItemCardLong/>
+        <ItemCardLong/>
+    </Stack>
+</Stack>)
 
 const Footer = () => (<Stack direction={"horizontal"} className={"justify-content-around"}>
     <Stack gap={3}>
@@ -88,12 +181,16 @@ const Footer = () => (<Stack direction={"horizontal"} className={"justify-conten
 </Stack>)
 
 const PizzaHut = () => (<>
-    <Navbar/>
+    <Navigation/>
     <Container>
-        <CenterImage/>
-        <StartHere/>
-        <hr/>
-        <Footer/>
+        <Stack gap={5} className={"my-5"}>
+            <CenterImage/>
+            <StartHere/>
+            <Featured/>
+            <hr/>
+            <Footer/>
+            <hr/>
+        </Stack>
     </Container></>);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
